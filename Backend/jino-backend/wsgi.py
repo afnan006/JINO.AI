69cbafc22e6e1c3bc783a4e7454a65c52ad66fe1
afnan006 <afnan006cs@gmail.com>
2025-04-21 09:46:38 +0530

    Backend--Production--Ready-for-MVP-v1.0-...

--- Backend/jino-backend/wsgi.py
+++ Backend/jino-backend/wsgi.py
@@ -1,2 +1,6 @@
-from app import create_app
-app = create_app()
+from app import create_app  # Import create_app function from app/__init__.py
+
+app = create_app()  # Create the app instance
+
+if __name__ == "__main__":
+    app.run()
