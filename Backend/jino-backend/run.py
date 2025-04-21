87bd3c702e1ed44a942118b92eb9f717d2f145db
afnan006 <afnan006cs@gmail.com>
2025-04-21 07:28:24 +0530

    Backend Production Ready for MVP v1.0

--- Backend/jino-backend/run.py
+++ Backend/jino-backend/run.py
@@ -1,6 +1,15 @@
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Defaults to 5000 if PORT is not set
+    app.run(debug=True, host='0.0.0.0', port=port)
