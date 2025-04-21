09394d9d1cfc232d093920d01bf19a157244e36c
afnan006 <afnan006cs@gmail.com>
2025-04-21 10:58:43 +0530

    Frontend-Backend-Production-MVP-v1.0

--- Backend/jino-backend/run.py
+++ Backend/jino-backend/run.py
@@ -8,5 +8,5 @@ from waitress import serve
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     serve(app, host='0.0.0.0', port=port)  # Start the server with Waitress
--- Backend/jino-backend/wsgi.py
+++ Backend/jino-backend/wsgi.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # default for local dev
+    port = int(os.environ.get("PORT", 10000))  # default for local dev
     app.run(host="0.0.0.0", port=port)
